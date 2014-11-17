caedc57c5af6ae0d3b40bbcc18f7247fde1f57ed
sdwarak <dwarakanath.soundararajan@gmail.com>
2014-11-17 18:31:59 -0500

    Ruby test 1

---
+++ ruby_array_test.rb
@@ -0,0 +1,13 @@
+arrayOne = Array.new
+arrayTwo = Array.new
+i=0
+resultArray =[]
+arrayOne = gets.chomp
+arrayTwo = gets.chomp
+
+while array_one.length
+	resultArray[i] = arrayOne[i].to_i + arrayTwo[i].to_i
+  	x += 1
+end	
+
+puts resultArray
